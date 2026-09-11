57854a86ddf3c06c2303f4d2ead00db2dc6837ec
AndreaGiorgino <andreagiorgino1@gmail.com>
2026-09-11 13:59:13 +0200

    exe template updated

--- exe/build.sh
+++ exe/build.sh
@@ -1,4 +1,86 @@
 #!/usr/bin/env bash
 
-cmake -B build \
-    && cmake --build build --parallel $(($(nproc) - 1))
+usage='''NAME
+    build - exename build script
+
+SYNOPSIS
+    build [options...] [target]
+
+DESCRIPTION
+    exename script for target builds. If no target has been provided,
+    then ''build'' is used.
+
+OPTIONS
+    -v, --version
+        Print the script version message and exit.
+
+    -h, --help
+        Print the script help message and exit.
+
+    --autorun
+        Autorun after build based on the target.
+
+TARGETS
+    build
+        Build exename.
+
+    debug
+        Build exename in debug mode.
+'''
+
+version='1.0.0'
+
+while true; do
+    case $1 in
+        -h | --help)
+            echo "${usage}"
+            exit 0
+            ;;
+        -v | --version)
+            echo "build v${version}"
+            exit 0
+            ;;
+        --autorun)
+            autorun=true
+            shift;
+            ;;
+        --)
+            shift;
+            break;
+            ;;
+        -*)
+            echo "Invalid option '${1}'"
+            echo ''
+            echo "${usage}"
+            exit 2
+            ;;
+        *)
+            break;
+            ;;
+    esac
+done
+
+case $1 in
+    '' | build)
+        cmake -B build && \
+            cmake --build build --parallel $(($(nproc) - 1))
+
+        [[ $? == 0 && $autorun ]] && \
+            ./build/exename
+        ;;
+    debug)
+        cmake -B build -DDEBUG=ON && \
+            cmake --build build --parallel $(($(nproc) - 1))
+
+        [[ $? == 0 && $autorun ]] && \
+            ./build/exename
+        ;;
+    *)
+        echo "Invalid target '${1}'"
+        echo ''
+        echo "${usage}"
+        exit 2
+        ;;
+esac
+
+exit $?
